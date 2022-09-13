2c23ee2e19c73c980b36d2c095e7121990f598a7
山崎仁 <yamazakihitoshi@yamazakihitoshinoMacBook-Pro-2.local>
2022-09-13 14:16:43 +0900

    SheetTagBuilder のメンバの初期化処理

--- src/main/java/io/luchta/forma4j/reader/model/tag/SheetTag.java
+++ src/main/java/io/luchta/forma4j/reader/model/tag/SheetTag.java
@@ -72,7 +72,10 @@ public class SheetTag implements Tag {
         /**
          * コンストラクタ
          */
-        public SheetTagBuilder() {}
+        public SheetTagBuilder() {
+            name = new Name();
+            allSheets = new AllSheets();
+        }
 
         /**
          * name プロパティを設定するメソッドです
