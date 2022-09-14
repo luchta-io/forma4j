f6d90209b7c8ff71e516d551dbdc21236df93231
山崎仁 <yamazakihitoshi@yamazakihitoshinoMacBook-Pro-2.local>
2022-09-14 17:28:31 +0900

    TAG_NAME を追加

--- src/main/java/io/luchta/forma4j/reader/model/tag/FormaReaderTag.java
+++ src/main/java/io/luchta/forma4j/reader/model/tag/FormaReaderTag.java
@@ -1,7 +1,18 @@
 package io.luchta.forma4j.reader.model.tag;
 
+/**
+ * {@code FormaReaderTag} は forma-reader タグを扱うためのクラスです
+ */
 public class FormaReaderTag implements Tag {
+    /**
+     * 設定ファイルに記述されるタグの名称です
+     */
+    public static final String TAG_NAME = "forma-reader";
 
+    /**
+     * タグが forma-reader タグかどうかを返します
+     * @return true: forma-reader タグ, false: forma-reader タグではない
+     */
     @Override
     public boolean isFormaReader() {
         return true;
