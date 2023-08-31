353c2f9126de5c9109de1f3937c7881dd0e0d4b9
Kiminobu Taniguchi <k_taniguchi@aciter.co.jp>
2023-08-31 17:41:33 +0900

    POIのCellUtil.setCellStylePropertyを使わないようにして、セルの背景色が期待通りになるようにする

--- src/main/java/io/luchta/forma4j/writer/processor/poi/setting/StyleSetting.java
+++ src/main/java/io/luchta/forma4j/writer/processor/poi/setting/StyleSetting.java
@@ -5,23 +5,37 @@ import io.luchta.forma4j.writer.engine.model.cell.style.XlsxCellStyle;
 import io.luchta.forma4j.writer.engine.model.cell.style.XlsxCellStyles;
 import org.apache.poi.ss.usermodel.BorderStyle;
 import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.util.CellUtil;
+import org.apache.poi.ss.usermodel.CellStyle;
 
 public class StyleSetting {
+
     public void set(Cell cell, XlsxCellStyles styles) {
-        CellUtil.setCellStyleProperty(cell, CellUtil.WRAP_TEXT, true);
+        baseStyle(cell);
+        customStyle(cell, styles);
+    }
+
+    private void baseStyle(Cell cell) {
+        CellStyle defaultStyle = cell.getCellStyle();
+        // FIXME cellごとにCellStyleを作ると、ワークブック内のCellStyleの上限に引っかかるのでこのままではまずい refs.https://qiita.com/Ky01_dev/items/efeca05faaf08bd4fd41
+        CellStyle newStyle = cell.getSheet().getWorkbook().createCellStyle();
+        newStyle.cloneStyleFrom(defaultStyle);
+        newStyle.setWrapText(true);
+        cell.setCellStyle(newStyle);
+    }
+
+    private void customStyle(Cell cell, XlsxCellStyles styles) {
         for (XlsxCellStyle style : styles) {
-            if (style.isBorder()) {
-                border(cell, (XlsxBorderStyle) style);
-            }
+            if (style.isBorder()) border(cell, (XlsxBorderStyle) style);
         }
     }
 
     private void border(Cell cell, XlsxBorderStyle style) {
+        CellStyle cellStyle = cell.getCellStyle();
+
         BorderStyle borderStyle = style.getBorderStyle();
-        CellUtil.setCellStyleProperty(cell, CellUtil.BORDER_LEFT, borderStyle);
-        CellUtil.setCellStyleProperty(cell, CellUtil.BORDER_TOP, borderStyle);
-        CellUtil.setCellStyleProperty(cell, CellUtil.BORDER_RIGHT, borderStyle);
-        CellUtil.setCellStyleProperty(cell, CellUtil.BORDER_BOTTOM, borderStyle);
+        cellStyle.setBorderLeft(borderStyle);
+        cellStyle.setBorderTop(borderStyle);
+        cellStyle.setBorderRight(borderStyle);
+        cellStyle.setBorderBottom(borderStyle);
     }
 }
