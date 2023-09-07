0c12fed8a1a0c8f3f983925613d5fd17fba5150c
Kiminobu Taniguchi <k_taniguchi@aciter.co.jp>
2023-09-07 14:49:55 +0900

    interfaceのJavadocを追記

--- src/main/java/io/luchta/forma4j/writer/engine/model/cell/style/XlsxCellStyleProperty.java
+++ src/main/java/io/luchta/forma4j/writer/engine/model/cell/style/XlsxCellStyleProperty.java
@@ -3,5 +3,11 @@ package io.luchta.forma4j.writer.engine.model.cell.style;
 import org.apache.poi.ss.usermodel.CellStyle;
 
 public interface XlsxCellStyleProperty {
+
+    /**
+     * 引数で渡された {@code CellStyle} に対して、このプロパティの値を上書きします。
+     *
+     * @param cellStyle 上書き対象の {@code CellStyle}
+     */
     void overwriteTo(CellStyle cellStyle);
 }
